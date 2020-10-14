684563d15f3da44ba776453ce7e566ca6dade816
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-10-13 23:49:08 -0700

    Added particles to the page

--- main.js
+++ main.js
@@ -61,11 +61,83 @@ function handleThemeChange() {
     document.body.className = 'light';
     textElement.className = 'dark-text';
     textElement.textContent = 'Switch to dark theme';
+    loadParticles('#638693', 0.7);
   } else {
     document.body.className = 'dark';
     textElement.className = 'light-text';
     textElement.textContent = 'Switch to light theme';
+    loadParticles('#EFEFEF', 0.4);
   };
 };
 
+function loadParticles(color, opacity) {
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 355,
+        "density": {
+          "enable": true,
+          "value_area": 789
+        }
+      },
+      "color": {
+        "value": `${color}`,
+      },
+      "shape": {
+        "type": "circle",
+      },
+      "opacity": {
+        "value": opacity,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 1,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2.5,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 2,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": false,
+        },
+        "resize": true
+      },
+    },
+    "retina_detect": true
+  });
+};
+
+loadParticles('#EFEFEF', 0.4);
 startClock();
